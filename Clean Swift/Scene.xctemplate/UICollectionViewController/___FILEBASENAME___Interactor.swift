1a1504cdcd88bdc3a2b1a5a4535d133db57d7028
Mads Kleemann <madskleemann@me.com>
2016-11-07 15:42:58 +0100

    Initial commit

---
+++ Clean Swift/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
+    func presentSomething(response: ___FILEBASENAMEASIDENTIFIER___Response)
+}
+
+class ___FILEBASENAMEASIDENTIFIER___Interactor: ___FILEBASENAMEASIDENTIFIER___ViewControllerOutput  {
+    var output: ___FILEBASENAMEASIDENTIFIER___InteractorOutput!
+    var worker: ___FILEBASENAMEASIDENTIFIER___Worker!
+    
+    // MARK: Business logic
+    
+    func doSomething(request: ___FILEBASENAMEASIDENTIFIER___Request) {
+        // NOTE: Create some Worker to do the work
+        
+        worker = ___FILEBASENAMEASIDENTIFIER___Worker()
+        worker.doSomeWork()
+        
+        // NOTE: Pass the result to the Presenter
+        
+        let response = ___FILEBASENAMEASIDENTIFIER___Response()
+        output.presentSomething(response: response)
+    }
+}
